4a95488ab7eec88c58bbe07109791c36315dd3e4
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-06-28 22:33:58 +0000

    create reusable ChargingStationRadialWidget

--- lib/src/ui/widgets/radial_widget.dart
+++ lib/src/ui/widgets/radial_widget.dart
@@ -33,3 +33,46 @@ class RadialWidget extends StatelessWidget {
     );
   }
 }
+
+class ChargingStationRadialWidget extends StatelessWidget {
+  const ChargingStationRadialWidget({
+    super.key,
+    required this.constraints,
+    required this.isCharging,
+  });
+
+  final BoxConstraints constraints;
+  final bool isCharging;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = constraints.maxWidth * 1.4;
+    final radius = width / 2;
+
+    return Container(
+      height: width,
+      width: width,
+      decoration: BoxDecoration(
+        //   color: Colors.red,
+
+        gradient: RadialGradient(
+          colors: !isCharging
+              ? [
+                  Colors.black,
+                  Colors.black,
+                  Colors.black.withOpacity(0.4),
+                  Colors.black.withOpacity(0.3),
+                  const Color(0xFFD9D9D9).withOpacity(0.1),
+                ]
+              : [
+                  Colors.black,
+                  Colors.black,
+                  Colors.black,
+                  Color(0xFFD9D9D9),
+                ],
+        ),
+        borderRadius: BorderRadius.circular(radius),
+      ),
+    );
+  }
+}
